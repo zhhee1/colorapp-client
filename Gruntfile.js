632c90223199b028744f03457b27325efac26e9c
zhhee1 <zhhee1@student.monash.edu>
2017-01-12 16:05:11 +0800

    updates

--- Gruntfile.js
+++ Gruntfile.js
@@ -70,9 +70,9 @@ module.exports = function (grunt) {
     // The actual grunt server settings
     connect: {
       options: {
-        port: '0.0.0.0',
+        port: '9000',
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost',
+        hostname: 'colorapp-client.herokuapp.com',
         livereload: 35729
       },
       livereload: {
