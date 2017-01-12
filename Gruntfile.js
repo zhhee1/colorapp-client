92143d91057ca93ff9c56f469ab8757d5e47af43
zhhee1 <zhhee1@student.monash.edu>
2017-01-12 15:25:27 +0800

    updates

--- Gruntfile.js
+++ Gruntfile.js
@@ -70,7 +70,7 @@ module.exports = function (grunt) {
     // The actual grunt server settings
     connect: {
       options: {
-        port: '127.0.0.1',
+        port: '9000',
         // Change this to '0.0.0.0' to access the server from outside.
         hostname: 'localhost',
         livereload: 35729
